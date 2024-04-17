a753179ef0c3c35065eaa7289206304475b2b7df
journnie <jhyun095@gmail.com>
2024-04-17 17:18:41 +0900

    js basic

---
+++ basic_javascript/practice3/skeleton_3.js
@@ -0,0 +1,17 @@
+// 아래에 코드 작성
+const person = {
+    name: 'Alice',
+    age: 30,
+    city: 'New York',
+    introduce() {
+        return `안녕하세요 ${this.city}에 거주하는 ${this.age}살 ${this.name}입니다.`;
+    }
+}
+
+const printPersonInfo = function ({ name, age, city }) {
+    console.log(`${name} / ${age} / ${city}`)
+}
+
+// 메서드 및 함수 호출
+console.log(person.introduce())  // 안녕하세요 New York에 거주하는 30살 Alice입니다.
+printPersonInfo(person)  // Alice / 30 / New York
